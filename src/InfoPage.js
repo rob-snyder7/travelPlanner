2e38aa0b2abb2e21d4b5db5c3da1bb462c2cf0f2
Rob Snyder <rksnyder7@gmail.com>
2018-08-12 13:19:16 -0700

    componenets functional (attractions, cities, and google maps)

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import { countryAttractions, countryAttractionsImages, countryCities, countryCitiesImages } from './Data';
+import ScrollBar from 'react-perfect-scrollbar';
+import './InfoPage.css';
+import GoogleMap from './GoogleMap';
+
+
 
 const InfoPage = ({country, onBack}) => {
+
+	let attractions = []
+	let cities = []
+	const numbToDisplay = [0,1,2,3,4,5,6,7,8,9]
+
+	numbToDisplay.forEach(function(element) {
+		attractions.push(<div> {countryAttractions[country][element]} </div>)
+		attractions.push(<img className='img-responsive' src={countryAttractionsImages[country][element]} />)
+	})
+
+	numbToDisplay.forEach(function(element) {
+		cities.push(<div> {countryCities[country][element]} </div>)
+		cities.push(<img className='img-responsive' src={countryCitiesImages[country][element]} />)
+	})
+
+	const hold = countryAttractions['Austria'][0]
+
 	return (
-		<div>
+
+		<div className='vh-100'>
+	
 			{country}
 				<div className="container">
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-4 vh-75">
 						top attractions
+						<ScrollBar>
+							{attractions}
+						</ScrollBar>
 						</div>
-						<div className="col-sm-8">
-						gallery
+						
+						
+						<div className="col-sm-4 vh-75">
+						top cities
+						<ScrollBar>
+							{cities}
+						</ScrollBar>		
+						</div>
+						<div className="col-sm-4">
+						<GoogleMap />
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-4 col-xs-offset-8">
 						top cities
-						</div>
-						<div className="col-sm-4">
-						cost (hostels, food)
-						</div>
-						<div className="col-sm-4">
-						flight prices
 						</div>
 					</div>
 				</div>
@@ -34,41 +64,3 @@ const InfoPage = ({country, onBack}) => {
 }
 
 export default InfoPage;
-
-// <Grid>
-//   <Row className="show-grid">
-//     <Col xs={12} md={8}>
-//       <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
-//     </Col>
-//     <Col xs={6} md={4}>
-//       <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
-//     </Col>
-//   </Row>
-
-//   <Row className="show-grid">
-//     <Col xs={6} md={4}>
-//       <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
-//     </Col>
-//     <Col xs={6} md={4}>
-//       <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
-//     </Col>
-//     <Col xsHidden md={4}>
-//       <code>&lt;{'Col xsHidden md={4}'} /&gt;</code>
-//     </Col>
-//   </Row>
-
-//   <Row className="show-grid">
-//     <Col xs={6} xsOffset={6}>
-//       <code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code>
-//     </Col>
-//   </Row>
-
-//   <Row className="show-grid">
-//     <Col md={6} mdPush={6}>
-//       <code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code>
-//     </Col>
-//     <Col md={6} mdPull={6}>
-//       <code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code>
-//     </Col>
-//   </Row>
-// </Grid>;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'tachyons';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import registerServiceWorker from './registerServiceWorker';
 
 

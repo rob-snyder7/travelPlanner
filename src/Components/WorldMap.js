7bbebf2a7032b72bc3a257b28e838344e357ec43
Rob Snyder <rksnyder7@gmail.com>
2018-08-19 20:13:40 -0700

    adding tooltip to worldMap

--- src/Components/WorldMap.js
+++ src/Components/WorldMap.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import withRedux from "next-redux-wrapper"
 import { ComposableMap, ZoomableGroup, Geographies, Geography } from "react-simple-maps";
 import { WorldMapData } from '../Utils/Data';
+import { connect } from 'react-redux';
+import {
+  Tooltip,
+  actions,
+} from "redux-tooltip"
 
+import { initStore } from './stores';
+
+const { show, hide } = actions
 
 class WorldMap extends Component {
+  constructor() {
+    super()
+    this.handleMove = this.handleMove.bind(this)
+    this.handleLeave = this.handleLeave.bind(this)
+  }
+  handleMove(geography, evt) {
+    const x = evt.clientX
+    const y = evt.clientY + window.pageYOffset
+    this.props.dispatch(
+      show({
+        origin: { x, y },
+        content: geography.properties.name,
+      })
+    )
+  }
+  handleLeave() {
+    this.props.dispatch(hide())
+  }	
 
 	render() {
 		return (
@@ -12,6 +39,7 @@ class WorldMap extends Component {
 					<p className="black b f1 f-headline-ns tc db mb3 mb4-ns" title="Home">Travel Planner</p>
 					<p className='black f2'>select dream destination below</p>
 				</div>
+			<div>
 				<ComposableMap className='ba' style={{width: '90%', height: 'auto', maxHeight: '70vh'}}>
 				  <ZoomableGroup>
 				    <Geographies  geography={ WorldMapData }>
@@ -20,6 +48,8 @@ class WorldMap extends Component {
 				        	key={ geography.id } 
 				        	geography={ geography } 
 				        	projection={ projection } 
+		                    onMouseMove={this.handleMove}
+		                    onMouseLeave={this.handleLeave}
 				        	style={{
 			                    default: {			   
 			                      fill: "#ECEFF1",
@@ -46,9 +76,12 @@ class WorldMap extends Component {
 				    </Geographies>
 				  </ZoomableGroup>
 				</ComposableMap>
+				<Tooltip />
+				</div>
 			</div>
 		)
 	}
 }
 
-export default WorldMap;
+
+export default withRedux(initStore)(WorldMap);
